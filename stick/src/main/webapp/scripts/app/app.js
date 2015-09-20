7ccf92d6abd6b25d24e121633b3d07f2c8def2b9
Brian O'Connor <briandoconnor@gmail.com>
2015-09-20 13:26:56 -0400

    Not sure these changes are even important

--- stick/src/main/webapp/scripts/app/app.js
+++ stick/src/main/webapp/scripts/app/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('stickApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalprecht.translate', 
+angular.module('stickApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalprecht.translate',
                'ui.bootstrap', // for modal dialogs
-    'ngResource', 'ui.router', 'ngCookies', 'ngCacheBuster', 'ngFileUpload', 'infinite-scroll'])
+    'ngResource', 'ui.router', 'ngCookies', 'ngCacheBuster', 'ngFileUpload', 'infinite-scroll', 'satellizer'])
 
     .run(function ($rootScope, $location, $window, $http, $state, $translate, Language, Auth, Principal, ENV, VERSION) {
         $rootScope.ENV = ENV;
@@ -14,12 +14,12 @@ angular.module('stickApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalpr
             if (Principal.isIdentityResolved()) {
                 Auth.authorize();
             }
-            
+
             // Update the language
             Language.getCurrent().then(function (language) {
                 $translate.use(language);
             });
-            
+
         });
 
         $rootScope.$on('$stateChangeSuccess',  function(event, toState, toParams, fromState, fromParams) {
@@ -32,12 +32,12 @@ angular.module('stickApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalpr
             if (toState.data.pageTitle) {
                 titleKey = toState.data.pageTitle;
             }
-            
+
             $translate(titleKey).then(function (title) {
                 // Change window title with translated one
                 $window.document.title = title;
             });
-            
+
         });
 
         $rootScope.back = function() {
@@ -79,7 +79,7 @@ angular.module('stickApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalpr
         $httpProvider.interceptors.push('authExpiredInterceptor');
         $httpProvider.interceptors.push('authInterceptor');
         $httpProvider.interceptors.push('notificationInterceptor');
-        
+
         // Initialize angular-translate
         $translateProvider.useLoader('$translatePartialLoader', {
             urlTemplate: 'i18n/{lang}/{part}.json'
@@ -93,5 +93,5 @@ angular.module('stickApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalpr
         tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js');
         tmhDynamicLocaleProvider.useCookieStorage();
         tmhDynamicLocaleProvider.storageKey('NG_TRANSLATE_LANG_KEY');
-        
+
     });
